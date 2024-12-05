69e425ed85ba0d4c9fd4ea16a1af8b3884e721fe
Erremme <Erremme2.0@gmail.com>
2024-12-05 10:39:49 +0100

    fix: function pali exercise

--- pali/script.js
+++ pali/script.js
@@ -4,27 +4,24 @@
  
 
 //1 funzione
-function AreElementSame ( wort1, wort2){
+function AreElementSame ( wort){
  //2qusta funzione deve verificare se la parola inserita dallo user è palindroma
- for(let i = wort1.length -1; i >= 0; i--){
-    wort2 += wort1[i]
-    let isElementPali =false
-    if( wort1 === wort2){
-        isElementPali = true;
-        return isElementPali;
-        
+ let UserwortReverse = "";
+ for(let i = wort.length -1; i >= 0; i--){
+    UserwortReverse += wort[i]
+    
+}
+    if( wort === UserwortReverse){
+        return true      
  }
+    return false;
 }
-}
+
 
 //3 faccio inserire la parola allo user
 const Userwort = prompt( "Inserisci una parola")
-//4 Inializzo una variabile con una stringa vuota
-let UserwortReverse = "";
-//5 inizializzo una variabile di default con valore false
-let IsPaliWort= false;
-//6 utilizzo la mia funzione
-IsPaliWort = AreElementSame( Userwort , UserwortReverse)
+//4 utilizzo la mia funzione
+IsPaliWort = AreElementSame( Userwort )
 
 if(IsPaliWort){
     alert("La parola che hai inserito è palindroma")
